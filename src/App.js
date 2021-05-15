1b257d5a3580612ae2e2f48cca3cdf3ac5f9cb9f
Patricia Steinberg <patriciacsteinberg@gmail.com>
2021-05-15 11:32:31 -0400

    First route

--- src/App.js
+++ src/App.js
@@ -1,14 +1,23 @@
 import React from 'react';
 import './App.css';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+import Home from './Components/Home';
+import Header from './Components/Header';
+
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route></Route>
-      </Routes>
-    </BrowserRouter>
+    <Router>
+        <Header />
+        <main>
+          <Route path="/" component={Home} />
+        </main>
+    </Router>
   );
 }
 
