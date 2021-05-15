1dcab4ed91b08bf1309b2ea7dc27812a37ae7a3f
Patricia Steinberg <patriciacsteinberg@gmail.com>
2021-05-15 16:52:14 -0400

    fixing type-o

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -5,7 +5,7 @@ const Home = () => {
     return (
         <div className={`${styles.homeContent} container`}>
             <h1 className={styles.title}>Patricia Steinberg</h1>
-            <h2>Front-end develper</h2>
+            <h2>Front-end developer</h2>
             <ul>
                 <li><a href="https://www.linkedin.com/in/patricia-steinberg-74a6b6177" target="_blank"><img src="../Assets/home-linkedin-btn.png" alt="LinkedIn Profile Page"/></a></li>
                 <li><a href="https://www.behance.net/patriciasteinberg" target="_blank"><img src="../Assets/home-behance-btn.png" alt="Behance Profile Page"/></a></li>
