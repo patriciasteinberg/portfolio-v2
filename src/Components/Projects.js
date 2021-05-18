5e66990f2bb82dfb854d929e62eb8d8a3705df02
Patricia Steinberg <patriciacsteinberg@gmail.com>
2021-05-18 18:29:36 -0400

    Projects page, adding slider

--- src/Components/Projects.js
+++ src/Components/Projects.js
@@ -1,9 +1,12 @@
-import React from 'react'
+import React from "react";
+import Slide from "./Slide";
+import { SliderData } from "./SliderData";
 
 const Projects = () => {
     return (
         <div>
             <h1>My Projects</h1>
+            <Slide slides={SliderData} />
         </div>
     )
 }
