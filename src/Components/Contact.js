35bd49d68d7953df3749b8ab82272d28bd81b8a7
Patricia Steinberg <patriciacsteinberg@gmail.com>
2021-05-24 14:43:03 -0400

    Form submit and reCAPTCHA

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -3,17 +3,32 @@ import Button from './Button';
 import Input from './Input';
 import Message from './Message';
 import PageTitle from './PageTitle';
+import { useForm, ValidationError } from '@formspree/react';
+import styles from "./Contact.module.css";
+import ReCAPTCHA from "react-google-recaptcha";
+
 
 const Contact = () => {
+    const [state, handleSubmit] = useForm("xknkrwkp");
+    
+    if (state.succeeded) {
+        return <p className={styles.success}>Your message has been sent!</p>;
+    }
+
+    function onChange(value) {
+        return <Button type="submit">Send</Button>
+      }
+
     return (
         <section className="container">
+
             <PageTitle>Contact</PageTitle>
-            <form>
+            <form onSubmit={handleSubmit}>
                 <Input label="Name" type="text" name="name"/>
                 <Input label="Phone" type="tel" name="phone"/>
-                <Input label="Email" type="email" name="email"/>
+                <Input label="Email" type="email" name="_replyto"/>
                 <Message label="Leave your message here:" type="text" name="message"/>
-                <Button>Send</Button>
+                <ReCAPTCHA sitekey="6LeuQusaAAAAAOiISiyHnQiTY4hhqnN1kM1ZsIoG" onChange={onChange} />
             </form>
         </section>
     )
